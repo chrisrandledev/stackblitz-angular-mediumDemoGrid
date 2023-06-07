98dbee1a267f295c00745f8b67865dd4fd7a6dae
chrisrandledev <117551889+chrisrandledev@users.noreply.github.com>
2023-06-07 17:30:20 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to the React.ts demo on StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <div className="game-board">
+        <div className="wall"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="box"></div>
+        <div className="box"></div> <div className="box"></div>
+        <div className="wall"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="box"></div>
+        <div className="box"></div> <div className="box"></div>
+        <div className="box"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="box"></div>
+        <div className="wall"></div> <div className="box"></div>
+        <div className="box"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="box"></div>
+        <div className="waall"></div> <div className="box"></div>
+        <div className="box"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="wall"></div>
+        <div className="wall"></div> <div className="wall"></div>
+      </div>
+    </div>
+  );
+}
